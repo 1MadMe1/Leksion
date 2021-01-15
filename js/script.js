8556c88ae04b57e16599d7cf8663503ca6a275e2
1MadMe1 <76034176+1MadMe1@users.noreply.github.com>
2021-01-15 08:17:53 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var num=356;
+console.log(num);
+var greeting="hasjdhsajfas"
+
+console.log(greeting)
+
+console.log( "dfuhsjfsda");
+alert(greeting)
+var x=10;
+var y=4;
+var linebreak="<br>";
+document.write(linebreak);
+document.write(x*y);
+document.write(linebreak);
+document.write(x-y);
+document.write(linebreak);
+document.write(x/y);
+document.write(linebreak);
+document.write(x%y);
+document.write(linebreak);
+document.write(x+y);
+document.write(linebreak);
+var a=3
+document.write(a+1)
+
+//&& dhe ||
+
+var a=true;
+var b=false;
+
+
+console.log(a && b) //nxjerr false
+console.log(a || b) //nxjerr true
